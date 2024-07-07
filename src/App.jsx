f20f54d0fd9e2d5db490dc7e4b4b0628ab84db14
Mohammed Wasique <79024441+mdwasique@users.noreply.github.com>
2024-07-08 02:59:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GanttChart from "./Chart/GanttChart";
+
+function App() {
+  return (
+    <>
+      <GanttChart />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Chart/GanttChart.jsx
@@ -0,0 +1,81 @@
+import { Gantt } from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
+import { getStartEndDateForProject, initTasks } from "../helper";
+import React from "react";
+
+const GanttChart = () => {
+  const [tasks, setTasks] = React.useState(initTasks());
+  const [isChecked, setIsChecked] = React.useState(true);
+  let columnWidth = 65;
+
+  const handleTaskChange = (task) => {
+    console.log("On date change Id:" + task.id);
+    let newTasks = tasks.map((t) => (t.id === task.id ? task : t));
+    if (task.project) {
+      const [start, end] = getStartEndDateForProject(newTasks, task.project);
+      const project =
+        newTasks[newTasks.findIndex((t) => t.id === task.project)];
+      if (
+        project.start.getTime() !== start.getTime() ||
+        project.end.getTime() !== end.getTime()
+      ) {
+        const changedProject = { ...project, start, end };
+        newTasks = newTasks.map((t) =>
+          t.id === task.project ? changedProject : t
+        );
+      }
+    }
+    setTasks(newTasks);
+  };
+
+  const handleTaskDelete = (task) => {
+    const conf = window.confirm("Are you sure about " + task.name + " ?");
+    if (conf) {
+      setTasks(tasks.filter((t) => t.id !== task.id));
+    }
+    return conf;
+  };
+
+  const handleProgressChange = async (task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+    console.log("On progress change Id:" + task.id);
+  };
+
+  const handleDblClick = (task) => {
+    alert("On Double Click event Id:" + task.id);
+  };
+
+  const handleClick = (task) => {
+    console.log("On Click event Id:" + task.id);
+  };
+
+  const handleSelect = (task, isSelected) => {
+    console.log(task.name + " has " + (isSelected ? "selected" : "unselected"));
+  };
+
+  const handleExpanderClick = (task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+    console.log("On expander click Id:" + task.id);
+  };
+
+  return (
+    <div>
+      <Gantt
+        tasks={tasks}
+        viewMode="Week"
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onClick={handleClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        ganttHeight={300}
+        columnWidth={columnWidth}
+      />
+    </div>
+  );
+};
+
+export default GanttChart;
